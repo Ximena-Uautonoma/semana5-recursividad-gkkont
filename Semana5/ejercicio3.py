697f055393b7ea7f79669538ab2b387b651b1fc8
gkkont <fernanda.schneider@cloud.uautonoma.cl>
2026-04-02 08:25:50 -0300

    Update ejercicio3.py

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -6,8 +6,19 @@ Debe implementar una versión iterativa y una recursiva.
 """
 
 def factorial_ciclo(n):
+    if n < 0:
+        print (f"error")
+    resultado = 1
+    for i in range (2, n + 1):
+        resultado *= i
+    return resultado
     pass
 
 
 def factorial_recursivo(n):
+    if n < 0
+        print (f"error")
+    if n == 1 
+        return 1
+    return n * factorial_recursivo (n-1)
     pass

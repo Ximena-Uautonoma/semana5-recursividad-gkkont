06708275246c7a667e840021dc7028cfa93d1989
gkkont <fernanda.schneider@cloud.uautonoma.cl>
2026-04-02 08:16:39 -0300

    Update

--- Semana5/ejercicio2.py
+++ Semana5/ejercicio2.py
@@ -8,14 +8,15 @@ Debe implementar:
 """
 
 def suma_ciclo(n):
-    """
-    Retorna la suma de los primeros n números usando un ciclo.
-    """
+    suma = 0
+    for i in range (1, n +1)
+        suma += 1
+    return suma
     pass
 
 
 def suma_recursiva(n):
-    """
-    Retorna la suma de los primeros n números usando recursividad.
-    """
+    if n == 0
+        return 0
+    return n+ suma_recursiva(n-1) 
     pass
